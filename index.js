519cc8d91ee597c0324a2628ecf25e1bdbcc1483
Conner Isaacs <isaacsc@purdue.edu>
2018-12-02 21:59:19 -0500

    resolved

--- index.js
+++ index.js
@@ -22,7 +22,19 @@ var classSchema = {
     classID:String,
     courseName: String,
     Instructor:String,
-    students:[String]
+    students:[String],
+    polls:[String],
+    pollQuestions:[String]
+}
+
+var pollSchema = {
+    pollID:String,
+    classID:String,
+    question:String,
+    correctAnswer:String,
+    options:[String],
+    studentsVoted:[String],
+    status:String
 }
 
 //Connect to User database
@@ -37,6 +49,11 @@ var instructor = conn2.model('instructor', instructorSchema);
 var conn3 = mongoose.createConnection('mongodb://cylthus:skittles1@ds137263.mlab.com:37263/classes');
 var course = conn3.model('course', classSchema);
 
+
+//connect to poll database
+var conn4 =  mongoose.createConnection('mongodb://cylthus:skittles1@ds123664.mlab.com:23664/lab6polls');
+var poll = conn4.model('poll', pollSchema);
+
 //create app
 const app = express();
 const server = app.listen(8000);
@@ -50,8 +67,17 @@ app.get('/', function(req, res) {
     res.sendFile('login.html', {root: __dirname })
 });
 
+app.get('/NewPoll', function(req, res) {
+    res.sendFile('NewPoll.html', {root: __dirname })
+});
+app.get('/CurrentPoll', function(req, res) {
+    res.sendFile('CurrentPoll.html', {root: __dirname })
+});
 app.get('/login', function(req, res) {
     res.sendFile('login.html', {root: __dirname })
+});
+app.get('/InstructorCourse', function(req, res) {
+    res.sendFile('InstructorCourse.html', {root: __dirname })
 });
 
 app.get('/CreateAccount', function(req, res) {
@@ -91,7 +117,7 @@ io.on('connection',(socket)=>{
         });
     }
   });
-socket.on("createNewCourse", (data)=>{
+  socket.on("createNewCourse", (data)=>{
     var id = Math.floor(Math.random()*6000);
     while(1){
         var found = false;
@@ -107,6 +133,7 @@ socket.on("createNewCourse", (data)=>{
 
     //Save course to course database
     data.classID = id;
+    data.polls = [];
     new course(data).save((error) => {
         if(error){
             console.log('oops! Could not save course');
@@ -124,6 +151,79 @@ socket.on("createNewCourse", (data)=>{
   })
 })
 
+socket.on("getPollStatus", (data)=>{
+    poll.findOne({pollID: data.pollID}, (err, info)=>{
+        socket.emit("returnPollInfo", info);
+    })
+})
+socket.on("createPoll", (data)=>{
+    var id = Math.floor(Math.random()*6000);
+    while(1){
+        var found = false;
+        poll.findOne({pollID: id}, (err, info)=>{
+            if(info){
+                var found = true;
+                id = Math.random()*6000;
+            }
+        })
+        if(!found)break;
+    }
+
+
+    //Save course to course database
+    data.pollID = id;
+    data.status = "current";
+    new poll(data).save((error) => {
+        if(error){
+            console.log('oops! Could not save course');
+        } else {
+           // conn3.close();
+        }
+    });
+    //update instructor's classID's variable in intructor database
+    course.findOneAndUpdate(
+  { "classID": data.classID },
+  { $push: { polls: id, pollQuestions:data.question } },
+  { new: true },
+  (err, updatedDoc) => {
+    // what ever u want to do with the updated document
+  })
+})
+socket.on("getPolls", (data)=>{
+    id = data.classID;
+    course.findOne({"classID": id}, (err, info)=>{
+        if(info){
+            socket.emit("returnPolls", info);
+        }
+    })
+})
+socket.on("endPoll", (data)=>{
+    console.log("ending poll: "+data.pollID);
+       poll.findOneAndUpdate(
+  { "pollID": data.pollID },
+  { $set: { status: "past"} },
+  { new: true },
+  (err, updatedDoc) => {
+    // what ever u want to do with the updated document
+  })
+})
+socket.on("checkForInstructor", (data)=>{
+    console.log("user's email: "+data.user);
+    poll.findOne({pollID: data.pollID}, (err, info)=>{
+        if(info){
+            course.findOne({classID: info.classID}, (err, x)=>{
+                console.log("Poll instructor: "+x.Instructor);
+                if(x.Instructor == data.user){
+                    socket.emit("pollEnded", {x:true});
+                }
+                else {
+                    console.log("Instructors not equal");
+                }
+            })
+        }
+    })
+})
+
   socket.on("login", (data)=>{
       Person.findOne({"email":data.email, "password":data.password}, (err,x)=>{
             if(x){
@@ -135,8 +235,6 @@ socket.on("createNewCourse", (data)=>{
   })
 
   socket.on("getInstructorClasses", (data)=>{
-      console.log("Getting classes");
-      console.log("Looking up email: "+data.email);
         instructor.findOne({"instructorEmail":data.email}, (err, info)=>{
            console.log("id's found:" + info.classIDs);
             if(info){
